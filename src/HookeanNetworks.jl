50d608683cdfb1855c5ed58f23fbba52d4231d62
Moisés López <moises.lop.0415@gmail.com>
2025-10-05 13:37:44 -0600

    Update HookeanNetworks.jl

--- src/HookeanNetworks.jl
+++ src/HookeanNetworks.jl
@@ -1,5 +1,5 @@
 module HookeanNetworks
-using LinearAlgebra,Statistics,GLMakie
+using LinearAlgebra,Statistics,GLMakie,GeometryBasics
 
 export TriangLattice,ForceCalc,CentroMasa,RecordVideo,ReadState,InnerPolygons,CalcEnergies,RecordVideoWithPolygons
 
